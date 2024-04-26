97a37acf3a30c723a5b53032f74df580d64046c0
Matthew Altenburg <mattaltenburg@gmail.com>
2024-04-26 13:18:44 +1000

    created a mask in the dataloader

--- English2French/main.py
+++ English2French/main.py
@@ -2,20 +2,17 @@ import pandas as pd
 from torch.utils.data import Dataset, DataLoader
 from transformers import XLMRobertaTokenizer
 
+# get the data !wget https://raw.githubusercontent.com/southern-cross-ai/TranslationAI/main/English2French/eng_french.csv
+
 # Constants
 batch_size=2
 context_window=512
 
-
 class English2FrenchDataset(Dataset):
     def __init__(self, csv_file, tokenizer, context_window):
-        # Read the CSV file directly within the class constructor
-        self.dataframe = pd.read_csv(
-            csv_file,
-            usecols=['English words/sentences', 'French words/sentences']
-        )
+        self.dataframe = pd.read_csv(csv_file, usecols=['English words/sentences', 'French words/sentences'])
         self.tokenizer = tokenizer
-        self.max_length = max_length
+        self.max_length = context_window
 
     def __len__(self):
         return len(self.dataframe)
@@ -24,7 +21,6 @@ class English2FrenchDataset(Dataset):
         source = self.dataframe.iloc[idx]['English words/sentences']
         target = self.dataframe.iloc[idx]['French words/sentences']
 
-        # Encode the source and target text
         source_encoded = self.tokenizer.encode_plus(
             source,
             max_length=self.max_length,
@@ -32,7 +28,6 @@ class English2FrenchDataset(Dataset):
             truncation=True,
             return_tensors='pt'
         )
-
         target_encoded = self.tokenizer.encode_plus(
             target,
             max_length=self.max_length,
@@ -41,16 +36,23 @@ class English2FrenchDataset(Dataset):
             return_tensors='pt'
         )
 
+        # Generate masks for source and target
+        source_mask = (source_encoded['input_ids'].squeeze() != self.tokenizer.pad_token_id)
+        target_mask = (target_encoded['input_ids'].squeeze() != self.tokenizer.pad_token_id)
+
         return {
             'source_text': source,
-            'source_input_ids': source_encoded['input_ids'].flatten(),
+            'source_input_ids': source_encoded['input_ids'].squeeze(),
+            'source_mask': source_mask,
             'target_text': target,
-            'target_input_ids': target_encoded['input_ids'].flatten()
+            'target_input_ids': target_encoded['input_ids'].squeeze(),
+            'target_mask': target_mask
         }
 
 def main():
   # Tokenization
   tokenizer = XLMRobertaTokenizer.from_pretrained('xlm-roberta-base');
+  vocab_size = tokenizer.vocab_size
   # Data Preparation
   dataset = English2FrenchDataset('eng_french.csv', tokenizer, context_window)
   dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
@@ -60,8 +62,9 @@ def main():
   print(next(iterator)['target_text'])
   print(next(iterator)['source_input_ids'][0:10])
   print(next(iterator)['target_input_ids'][0:10])
-
+  print(len(next(iterator)['source_input_ids']))
+  print(len(next(iterator)['target_input_ids']))
+  print(next(iterator)['target_mask'])
 
 if __name__ == "__main__":
     main()
-
