a9118fd7d850003dd3ed19dd1e583226c845df7c
Matthew Altenburg <mattaltenburg@gmail.com>
2024-04-26 15:31:19 +1000

    added model class

--- English2French/main.py
+++ English2French/main.py
@@ -74,6 +74,46 @@ class PositionalEncoding(nn.Module):
         x = x + self.pe[:x.size(0), :]
         return self.dropout(x)
 
+class Model(nn.Module):
+    def __init__(self, 
+                 vocab_size, 
+                 embedding_size, 
+                 nhead, 
+                 num_encoder_layers, 
+                 num_decoder_layers, 
+                 dim_feedforward, 
+                 max_seq_length, 
+                 dropout=0.1
+                 ):
+        super(Model, self).__init__()
+        self.embedding_size = embedding_size
+        self.embedding = nn.Embedding(vocab_size, embedding_size)
+        self.pos_encoder = PositionalEncoding(embedding_size, dropout, max_seq_length)
+        self.transformer = nn.Transformer(embedding_size, 
+                                          nhead, 
+                                          num_encoder_layers, 
+                                          num_decoder_layers, 
+                                          dim_feedforward, 
+                                          dropout)
+        self.out = nn.Linear(embedding_size, vocab_size)
+
+    def forward(self, src, tgt, src_key_padding_mask, tgt_key_padding_mask, memory_key_padding_mask=None):
+        src = self.embedding(src) * math.sqrt(self.embedding_size)
+        src = self.pos_encoder(src)
+        
+        tgt = self.embedding(tgt) * math.sqrt(self.embedding_size)
+        tgt = self.pos_encoder(tgt)
+        
+        output = self.transformer(src, 
+                                  tgt, 
+                                  src_key_padding_mask=src_key_padding_mask, 
+                                  tgt_mask=None,
+                                  tgt_key_padding_mask=tgt_key_padding_mask, 
+                                  memory_key_padding_mask=memory_key_padding_mask
+                                  )
+        output = self.out(output)
+        return output
+
 
 def main():
   # Tokenization
