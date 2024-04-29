106b3ed27765a699de1bb851f4e1e4e2813e092f
Matthew Altenburg <mattaltenburg@gmail.com>
2024-04-29 14:31:28 +1000

    reverted bake to the original

--- English2French/config.py
+++ English2French/config.py
@@ -8,17 +8,16 @@ class ModelConfig:
     num_decoder_layers: int = 6
     dim_feedforward: int = 2048
     dropout: float = 0.1
-    vocab_size: int = None  # Default to None, but intended to be set
 
-def create_model_config(vocab_size: int):
-    """ Factory function to create a ModelConfig with the specified vocab size. """
-    return ModelConfig(vocab_size=vocab_size)
+@dataclass
+class TrainingConfig:
+    batch_size: int = 128
+    context_window: int = 512
+    csv_columns: list = ('English words/sentences', 'French words/sentences')
 
-################ Example of file usage
+############# Example of file usage
 # model_config = ModelConfig()  # Uses default values defined in the dataclass
 # training_config = TrainingConfig()  # Uses default values defined in the dataclass
 
-####################### You can still override defaults if necessary:
+# You can still override defaults if necessary:
 # custom_config = ModelConfig(embedding_size=256, dropout=0.2)
-# config_instance = create_model_config(vocab_size=10000, dropout=0.2)
-# config.dropout = 0.2 
