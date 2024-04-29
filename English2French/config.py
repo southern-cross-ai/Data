352f492c9e03a0a4f1517728f24094cfb7845f17
Matthew Altenburg <mattaltenburg@gmail.com>
2024-04-29 14:25:53 +1000

    add create_model_config for vocab size

--- English2French/config.py
+++ English2French/config.py
@@ -3,17 +3,16 @@ from dataclasses import dataclass
 @dataclass
 class ModelConfig:
     embedding_size: int = 512
-    nhead: int = 4
-    num_encoder_layers: int = 3
-    num_decoder_layers: int = 3
-    dim_feedforward: int = 1024
+    nhead: int = 8
+    num_encoder_layers: int = 6
+    num_decoder_layers: int = 6
+    dim_feedforward: int = 2048
     dropout: float = 0.1
+    vocab_size: int = None  # Default to None, but intended to be set
 
-@dataclass
-class TrainingConfig:
-    batch_size: int = 128
-    context_window: int = 512
-    csv_columns: list = ('English words/sentences', 'French words/sentences')
+def create_model_config(vocab_size: int):
+    """ Factory function to create a ModelConfig with the specified vocab size. """
+    return ModelConfig(vocab_size=vocab_size)
 
 ################ Example of file usage
 # model_config = ModelConfig()  # Uses default values defined in the dataclass
@@ -21,3 +20,5 @@ class TrainingConfig:
 
 ####################### You can still override defaults if necessary:
 # custom_config = ModelConfig(embedding_size=256, dropout=0.2)
+# config_instance = create_model_config(vocab_size=10000, dropout=0.2)
+# config.dropout = 0.2 
